865328019fb37b6b92e6ddd3721fb7e3336ede18
Hrishabh963 <hrishabhkumar634@gmail.com>
2023-09-23 15:34:09 +0530

    Changed todos.js and client.js according to review points

--- src/routes/todos.js
+++ src/routes/todos.js
@@ -3,93 +3,104 @@ const validator = require('../middlewares/yupValidator');
 const router = require('express').Router();
 
 //To fetch all todos
-router.get('/', async (req, res, next) => {
-  try {
-    const todos = await todoTable.findAll();
-    if (todos === undefined) {
-      const error = new Error('Error Fetching data');
-      throw error;
-    } else {
-      res.status(200).json(todos);
+router.get('/', async(req, res, next) => {
+    try {
+        const todos = await todoTable.findAll();
+        if (todos === undefined) {
+            const error = new Error('Error Fetching data');
+            error.status = 500;
+            throw error;
+        }
+        res.status(200).json(todos);
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 });
 
 //To add a new todo
-router.post('/', validator, async (req, res, next) => {
-  const todo = req.body;
-  try {
-    const todoSaved = await todoTable.create(todo);
-    res.status(201).json(todoSaved);
-  } catch (error) {
-    next(error);
-  }
+router.post('/', validator, async(req, res, next) => {
+    const todo = req.body;
+    try {
+        const todoSaved = await todoTable.create(todo);
+        res.status(201).json(todoSaved);
+    } catch (error) {
+        next(error);
+    }
 });
 
 //To get the todo with matching id
-router.get('/:id', async (req, res, next) => {
-  try {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      throw new Error('Parameter passed is not a number');
+router.get('/:id', async(req, res, next) => {
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            throw new Error('Parameter passed is not a number');
+        }
+        const todo = await todoTable.findByPk(id);
+        if (!todo) {
+            const error = new Error('Id not found');
+            error.status = 404;
+            throw error;
+        }
+        res.status(200).json(todo);
+    } catch (error) {
+        next(error);
     }
-    const todo = await todoTable.findByPk(id);
-    if (!todo) {
-      throw new Error('Id not found');
-    }
-    res.status(200).json(todo);
-  } catch (error) {
-    next(error);
-  }
 });
 
 //To update the todo with matching id
-router.put('/:id', validator, async (req, res, next) => {
-  try {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      throw new Error('Id is not a number');
-    }
-    const todoToBeUpdated = req.body;
-    const isUpdated = await todoTable.update(todoToBeUpdated, {
-      where: {
-        id: id,
-      },
-    });
+router.put('/:id', validator, async(req, res, next) => {
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            throw new Error('Id is not a number');
+        }
 
-    if (isUpdated[0] === 0) {
-      throw new Error('Id not found');
+        const todoToBeUpdated = req.body;
+
+        const isPresent = await todoTable.findByPk(id);
+        if (!isPresent) {
+            const error = new Error('Id not found');
+            error.status = 404;
+            throw error;
+        }
+        await todoTable.update(todoToBeUpdated, {
+            where: {
+                id: id,
+            },
+        });
+        const updatedTodo = await todoTable.findByPk(id);
+        res.status(200).json(updatedTodo);
+    } catch (error) {
+        next(error);
     }
-    const updatedTodo = await todoTable.findByPk(id);
-    res.status(200).json(updatedTodo);
-  } catch (error) {
-    next(error);
-  }
 });
 
 //Deletes the todo with specified id
-router.delete('/:id', async (req, res, next) => {
-  try {
-    const id = Number(req.params.id);
-    if (isNaN(id)) {
-      throw new Error('Id is not a number');
+router.delete('/:id', async(req, res, next) => {
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            throw new Error('Id is not a number');
+        }
+
+        const isPresent = await todoTable.findByPk(id);
+        if (!isPresent) {
+            const error = new Error('Id not found');
+            error.status = 404;
+            throw error;
+        }
+
+        await todoTable.destroy({
+            where: {
+                id: id,
+            },
+        });
+        res.status(200).json({
+            message: `Todo with id ${id} deleted successfully`,
+        });
+    } catch (error) {
+        next(error);
     }
-    const isDeleted = await todoTable.destroy({
-      where: {
-        id: id,
-      },
-    });
-    if (isDeleted === 0) {
-      throw new Error(`Id doesn't exist`);
-    }
-    res.status(200).json({
-      message: `Todo with id ${id} deleted successfully`,
-    });
-  } catch (error) {
-    next(error);
-  }
 });
 
 module.exports = router;
